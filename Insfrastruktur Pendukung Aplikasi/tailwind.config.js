4f794801bd45232a738bd8ebcbc9fb8e74bdd872
Rafliramdhani16 <rafliramdhani773@gmail.com>
2023-12-18 15:52:09 +0700

    Tugas kelompok IPA

---
+++ Insfrastruktur Pendukung Aplikasi/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    darkMode: 'class',
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat'],
+      },
+      colors: {
+        wpu: '#bada55',
+        kopi: '#c0ffee',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
